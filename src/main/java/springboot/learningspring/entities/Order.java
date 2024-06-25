8cf49dc0b8388c2e477113758aadec1c2a4c21bf
cirobt <cbtorres89@gmail.com>
2024-06-25 20:17:24 -0300

    OrderStatus enum

--- src/main/java/springboot/learningspring/entities/Order.java
+++ src/main/java/springboot/learningspring/entities/Order.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import springboot.learningspring.entities.enums.OrderStatus;
 
 @Entity
 @Table(name="tb_order")
@@ -21,16 +22,18 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss'Z'", timezone="GMT") // UTC
     private Instant moment;
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name="client_id") // Nome da chave estrangeira
     private User client;
 
     public Order() {}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -48,6 +51,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
@@ -82,6 +95,5 @@ public class Order implements Serializable {
             return false;
         return true;
     }
-
     
 }
